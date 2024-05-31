17a7f3122df351db3b2bb46939f914b63f5f23e1
Morteza_HH <morteza.110f110.2018@gmail.com>
2024-05-31 10:21:58 +0330

    Plain Vue + Typescript project created.

---
+++ src/router/routes.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router"
+import HomePage from "@/Pages/HomePage.vue"
+// import PageName from "@/pages/NotFound.vue"
+// import NotFound from "@/pages/NotFound.vue"
+
+const routes = [
+    {
+        path: "/",
+        component: HomePage,
+    },
+    // {
+    //     path: "/aPath",
+    //     component: PageName,
+    //     children: [
+    //         {
+    //             path: "/aPath/anotherPath",
+    //             component: ChildPageName,
+    //         },
+    //     ],
+    // },
+    // { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { URL, fileURLToPath } from "node:url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    server: {
+        port: 5000,
+        host: true,
+        hmr: {
+            host: "localhost",
+        },
+        proxy: {
+            "/api": "http://localhost:3000",
+        },
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        target: "esnext",
+    },
+    plugins: [vue()],
 })
