df5c7e0fb8b2b75527a97aa7a2d69d26d449f286
Morteza_HH <morteza.110f110.2018@gmail.com>
2024-06-01 11:31:24 +0330

    PrimeVue added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from "vue"
 import "./style.scss"
-import router from "@/router/routes.ts"
 import App from "./App.vue"
+import router from "@/router/routes.ts"
+import PrimeVue from "primevue/config"
+import "primeicons/primeicons.css"
+
+import "primevue/resources/themes/aura-light-green/theme.css"
 
 const app = createApp(App)
 app.use(router)
+app.use(PrimeVue)
 app.mount("#app")
